70e654bc82361d7d68abcc42b18c7135f572900d
Nathan <nathansilva406@gmail.com>
2023-12-07 22:56:46 -0300

    Separando responsabilidades de componentes

---
+++ src/Components/BuscarProdutos/index.jsx
@@ -0,0 +1,29 @@
+import { useQuery } from 'react-query';
+import Produtos from '../Produtos';
+
+const BuscarProdutos = () => {
+  const { data, isLoading, error } = useQuery('products', () =>
+    fetch(
+      'https://mks-frontend-challenge-04811e8151e6.herokuapp.com/api/v1/products?page=1&rows=8&sortBy=id&orderBy=DESC'
+    ).then((res) => res.json())
+  );
+
+  if (isLoading) return 'Carregando...';
+
+  if (error) return 'Ocorreu um erro!: ' + error.message;
+
+  const dados = data && data.products ? data.products : [];
+  console.log(dados);
+
+  return (
+    
+    <div className=" grid grid-rows-2  md:grid-cols-3 sm:grid-cols-2 lg:grid-cols-4 gap-4 my-28 justify-items-center">
+      {dados.map(item => (
+        <Produtos  key={item.id} name={item.name} photo={item.photo} price={item.price}/>
+    ))}
+    </div>
+
+  )
+};
+
+export default BuscarProdutos;
--- src/Components/Produtos/index.jsx
+++ src/Components/Produtos/index.jsx
@@ -1,47 +1,41 @@
-import { useQuery } from 'react-query';
 import { FiShoppingBag } from "react-icons/fi";
 import { useContext} from 'react';
 import { CartContext } from '../../Context/CartContext';
 
-const Produtos = () => {
+// eslint-disable-next-line react/prop-types
+const Produtos = ({id, photo, name, price}) => {
 
+  const { cart, setCart} = useContext(CartContext)
 
-  const { addCart} = useContext(CartContext)
+  const addCart = (novo) => {
+   // const existeProduto = cart.some(produto => produto.id === novo)
+     
+    return setCart(item => [...item, novo])
+  }
 
-
-  const { data, isLoading, error } = useQuery('products', () =>
-    fetch(
-      'https://mks-frontend-challenge-04811e8151e6.herokuapp.com/api/v1/products?page=1&rows=8&sortBy=id&orderBy=DESC'
-    ).then((res) => res.json())
-  );
-
-  if (isLoading) return 'Carregando...'; 
-
-  if (error) return 'Ocorreu um erro!: ' + error.message;
-
-  // Verifica se data existe e se data.products existe antes de acessá-lo
-  const dados = data && data.products ? data.products : [];
+  console.log(cart);
 
   return (
-    
-    <div className=" grid grid-rows-2  md:grid-cols-3 sm:grid-cols-2 lg:grid-cols-4 gap-4 my-28 justify-items-center">
-        {dados.map((item) => (
-          <div key={item.id} className='bg-white w-56 h-72 grid my-8 justify-items-center rounded-lg border border-slate-100 shadow-lg'>
-
-            <img src={item.photo} className='w-40 h-40 flex justify-center content-center'/>
-            <div className='flex bg-gray-400 h-12  '>
-              <h1 className='w-28 bg-orange-200 text-base font-light'>{item.name}</h1>
-              <p className='bg-black rounded-md text-white font-bold h-6 px-1 '>R${item.price}</p>
-            </div>
             
-            <div className='flex items-end w-full'>
-              <button className='bg-blue-800 text-white rounded-b-lg w-full h-8 flex justify-center items-center' onClick={addCart}> 
-                <FiShoppingBag className='mx-3'/> COMPRAR </button>
-            </div>
+    <div key={id} className='bg-white w-56 h-72 grid my-8 justify-items-center rounded-lg border border-slate-100 shadow-lg'>
 
-          </div>
-        ))}
+      <img src={photo} className='w-40 h-40 flex justify-center content-center'/>
+      <div className='flex bg-gray-400 h-12  '>
+        <h1 className='w-28 bg-orange-200 text-base font-light'>{name}</h1>
+        <p className='bg-black rounded-md text-white font-bold h-6 px-1 '>R${price}</p>
+      </div>
+      
+      <div className='flex items-end w-full'>
+        <button className='bg-blue-800 text-white rounded-b-lg w-full h-8 flex justify-center items-center' 
+          onClick={() => addCart({id, name, photo, price})}
+        >
+          
+          <FiShoppingBag className='mx-3'/> COMPRAR </button>
+      </div>
+
     </div>
+        
+    
   );
 };
 
