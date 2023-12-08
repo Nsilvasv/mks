136367d7fd87b7c719432ddf241f1eccaed1d10d
Nathan <nathansilva406@gmail.com>
2023-12-07 22:54:42 -0300

    rodapé

---
+++ src/Components/Footer/index.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return(
+        <div className="flex justify-center items-cente">
+            <p>MKS sistemas © Todos os direitos reservados</p>
+        </div>
+    )
+}
+
+export default Footer
